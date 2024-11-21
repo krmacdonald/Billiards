dcd3610e8a14e70f725ace78fe35ba58caa79a94
KMacdonald <kylemacdo01@gmail.com>
2024-11-21 09:40:24 -0500

    Texture function

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import pygame
 from OpenGL.GLU import *
 from OpenGL.GL import *
-from PIL import *
+from PIL import Image
 import camera
 
 FLOOR_WIDTH = 20
@@ -11,6 +11,11 @@ WALL_HEIGHT = 10
 WALL_LENGTH = 10
 CEILING_WIDTH = FLOOR_WIDTH
 CEILING_LENGTH = FLOOR_LENGTH
+
+ceiling_name = "ceiling.png"
+wall_name = "wall.png"
+ceiling_tex_name = None
+wall_tex_name = None
 
 running = True
 clock = 0
@@ -28,6 +33,11 @@ def init():
     clock = pygame.time.Clock()
     pygame.key.set_repeat(300, 50)  # Key repeat rate
     running = True
+    textArr = glGenTextures(2)  # Texture names for all three textures to create
+    ceiling_tex_name = textArr[0]
+    wall_tex_name = textArr[1]
+    createTexture(ceiling_tex_name, ceiling_name)
+    createTexture(wall_tex_name, wall_name)
 
 def main_loop():
     global running, clock, animate
@@ -36,6 +46,31 @@ def main_loop():
         createCeiling()
         createFloor()
     pass
+
+def display():
+    width = window_dimensions[0]
+    height = window_dimensions[1]
+
+    glViewport(0, 0, width, height)
+    camera.SetProjection()
+    glClearColor(0, 0, 0, 0)
+
+    glFlush()
+    
+    drawScene()
+    glFlush()
+
+
+def drawScene():
+    glEnable(GL_LIGHTING)
+
+    camera.placeCamera()
+
+
+    createWalls()
+    createCeiling()
+    createFloor()
+
 
 #TODO create four walls around floor
 def createWalls():
@@ -51,6 +86,15 @@ def createFloor(width, length, texture1, texture2):
 #TODO Create any textured ceiling
 def createCeiling():
     pass
+
+def createTexture(tName, fName):
+    img = Image.open(fName)
+    x = img.size[0]
+    y = img.size[1]
+    text = img.tobytes("raw")
+    glBindTexture(GL_TEXTURE_2D, tName)
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, x, y, 0, GL_RGB, GL_UNSIGNED_BYTE, text)
+
 
 def draw_plane(width, height, texture):
     glBindTexture(GL_TEXTURE_2D, texture)
