4f0e272f6fb1830c085fbc4434a66bfe44a275ac
KMacdonald <kylemacdo01@gmail.com>
2024-11-21 10:20:14 -0500

    initial scene and camera

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pygame
 from OpenGL.GLU import *
 from OpenGL.GL import *
 from PIL import Image
+import utils
 import camera
 
 FLOOR_WIDTH = 20
@@ -11,9 +12,10 @@ WALL_HEIGHT = 10
 WALL_LENGTH = 10
 CEILING_WIDTH = FLOOR_WIDTH
 CEILING_LENGTH = FLOOR_LENGTH
+FPS = 30.0
 
-ceiling_name = "ceiling.png"
-wall_name = "wall.png"
+ceiling_name = "BilliardTable/textures/ceiling.png"
+wall_name = "BilliardTable/textures/wall.png"
 ceiling_tex_name = None
 wall_tex_name = None
 
@@ -22,37 +24,55 @@ clock = 0
 animate = 0
 window_dimensions = (900, 600)
 
+CAM_ANGLE = 60.0
+CAM_NEAR = 0.01
+CAM_FAR = 500.0
+INITIAL_EYE = utils.Point(0, 3, 0)
+INITIAL_LOOK_ANGLE = 45
+camera = camera.Camera(CAM_ANGLE, window_dimensions[0]/window_dimensions[1], CAM_NEAR, CAM_FAR, INITIAL_EYE, INITIAL_LOOK_ANGLE)
+
 def main():
     init()
     main_loop()
+    return
 
 
 def init():
+    global ceiling_tex_name, wall_tex_name, clock
     pygame.init()
     screen = pygame.display.set_mode(window_dimensions, pygame.DOUBLEBUF|pygame.OPENGL)
     clock = pygame.time.Clock()
     pygame.key.set_repeat(300, 50)  # Key repeat rate
     running = True
+    clock = pygame.time.Clock()
+
     textArr = glGenTextures(2)  # Texture names for all three textures to create
     ceiling_tex_name = textArr[0]
     wall_tex_name = textArr[1]
     createTexture(ceiling_tex_name, ceiling_name)
     createTexture(wall_tex_name, wall_name)
 
+def keyboard(event):
+    global running, camera
+    key = event.key
+    if(key == 27):
+        running = False
+
 def main_loop():
     global running, clock, animate
     while running:
-        createWalls()
-        createCeiling()
-        createFloor()
-    pass
+        display()
+
+        pygame.display.flip()
+        clock.tick(FPS)
+    pygame.quit()
 
 def display():
     width = window_dimensions[0]
     height = window_dimensions[1]
 
     glViewport(0, 0, width, height)
-    camera.SetProjection()
+    camera.setProjection()
     glClearColor(0, 0, 0, 0)
 
     glFlush()
@@ -63,20 +83,24 @@ def display():
 
 def drawScene():
     glEnable(GL_LIGHTING)
-
+    glLightModeli(GL_LIGHT_MODEL_TWO_SIDE, GL_TRUE)
+    
     camera.placeCamera()
 
 
-    createWalls()
+    #createWalls()
+
+    glPushMatrix()
+    glRotate(90, 90, 0, 0)
+    glTranslate(0, 3, 0)
     createCeiling()
-    createFloor()
+    glPopMatrix()
+    #createFloor()
 
 
 #TODO create four walls around floor
 def createWalls():
-    gluBeginSurface()
-    
-    gluEndSurface()
+    pass
 
 #TODO Create checkerboard floor
 def createFloor(width, length, texture1, texture2):
@@ -85,7 +109,7 @@ def createFloor(width, length, texture1, texture2):
 
 #TODO Create any textured ceiling
 def createCeiling():
-    pass
+    draw_plane(5, 5, ceiling_tex_name)
 
 def createTexture(tName, fName):
     img = Image.open(fName)
@@ -102,8 +126,25 @@ def draw_plane(width, height, texture):
 
 
     glEnable(GL_TEXTURE_2D)
-
     glBegin(GL_QUADS)
+
+    x1 = width/2.0
+    y1 = height
+    x2 = -x1
+    y2 = 0
+
+    glTexCoord2f(0, 0)
+    glVertex3f(x2, y2, 0)
+    glTexCoord2f(4, 0)
+    glVertex3f(x1, y2, 0)
+    glTexCoord2f(4, 4)
+    glVertex3f(x1, y1, 0)
+    glTexCoord2f(0, 4)
+    glVertex3f(x2, y1, 0)
+
+    glEnd()
+    glDisable(GL_TEXTURE_2D)
+
     
 
 
